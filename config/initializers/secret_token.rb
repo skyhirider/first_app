3f09e7865004dc1fdb43a8d8ef459d46d1506870
Marek Andreansky <marek.andreansky@gmail.com>
2013-05-30 22:36:32 +0200

    auto generated rails corpus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c374c310276b3b497f0631b74dfc0357c963aea47da6506dd0143a72cc128ec07ce47209196dc05922e5dd764685307fd423ebca61d7a8779191546efca9d278'
